cf9dddf83336dd09c836bfa1300546ea08df9dbc
NinTanDao64 <nintandao64@yahoo.com>
2016-03-06 23:22:53 -0800

    Added files via upload

---
+++ Squares.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Squares {
+	public static void main(String[] args) {
+	DrawingPanel panel = new DrawingPanel(300, 200);
+        Graphics g = panel.getGraphics();
+		  panel.setBackground(Color.CYAN);
+		  g.setColor(Color.RED);
+		  for(int i=1; i<=5; i++) {
+		  		g.drawRect(50, 50, 20*i, 20*i);
+		  }
+		  g.setColor(Color.BLACK);
+		  g.drawLine(50, 50, 150, 150);
+	}
+}
